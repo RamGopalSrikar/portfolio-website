d2b59a908cc61564ec4ac6d56ef35188428217c1
srikar katakam <katakamsrikar96@gmail.com>
2021-03-27 13:04:51 -0400

    count changed

--- js/count.js
+++ js/count.js
@@ -8,9 +8,9 @@ function loadXMLDoc() {
     if (this.readyState == 4 && this.status == 200) {
       var response = JSON.parse(this.responseText);
         console.log(response)
-        document.getElementById('count1').innerHTML='View Count : ' + response['Body']['viewcount']
+        document.getElementById('count1').innerHTML='View Count : ' + response
     }
   };
-  xhttp.open("GET", "https://ed886ht9ca.execute-api.ca-central-1.amazonaws.com/v1/viewcount", true);
+  xhttp.open("GET", "https://vz9f1bcfgk.execute-api.us-east-1.amazonaws.com/v1", true);
   xhttp.send();
 }
